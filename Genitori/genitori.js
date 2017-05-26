1796023c3b35b9ff7cfc640e2b803531dbe4753b
pamelasiclari <pamela.sicl@gmail.com>
2017-05-26 12:07:45 +0200

    first version

---
+++ Genitori/genitori.js
@@ -0,0 +1,12 @@
+/**
+ * Created by pamelasiclari on 25/05/17.
+ */
+'use strict';
+//1)A partire dallo starter-kit noi vogliamo realizzare una single page application, cioè vogliamo caricare differenti view nella stessa pagina.
+//Per farlo, come spiegato a lezione, dobbiamo utilizzare il meccanismo di routing. AngularJS supporta il routing tra viste tramite
+// il servizio $route
+angular.module('myApp', [
+    'ngRoute']);
+
+//CONFIGURARE ROUTE PROVIDER -> E' quello che dice come vengono gestite le viste!
+
