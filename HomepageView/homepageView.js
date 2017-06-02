f097f13d8fc3495e3f848becddc1f6317cb5ebcf
pamelasiclari <pamela.sicl@gmail.com>
2017-06-03 00:34:55 +0200

    HOMEPAGE commenti + aggiustine varie

--- HomepageView/homepageView.js
+++ HomepageView/homepageView.js
@@ -1,3 +1,10 @@
 /**
  * Created by pamelasiclari on 02/06/17.
  */
+
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        //aggiungere funzione per sistemare il menu sopra fisso e anche
+    });
+});
