9aab1d88d740a4791ea4f5fdf6dba3fbad19b6c2
giuseppegullotta <giuseppegullotta1994@gmail.com>
2017-06-06 09:09:11 +0200

    Componente: UTENTI + UTENTI_SERVIZIO

---
+++ components/utenti/utenti-service.js
@@ -0,0 +1,58 @@
+/**
+ * Created by giuseppegullotta on 04/06/17.
+ */
+
+'use strict';
+
+// SERVIZIO IMPLEMENTAZIONE STATO UTENTE
+// SALVATAGGIO INFO E PRIMA REGISTRAZIONE
+
+angular.module('puzzle.utenti.utentiService', [])
+
+    .factory('utenti', function($firebaseArray) {
+        return {
+            registerLogin: function (userId, email) {
+
+                // AGGIUNTA UTENTE A ELENCO UTENTI
+                var ref = firebase.database().ref().child("utenti").child(userId);
+
+                ref.update({
+                    email: email,
+                    logged: true
+                });
+            },
+
+            registerLogout: function (userId)
+            {
+                var ref = firebase.database().ref().child("utenti").child(userId);
+                // LOGOUT
+                ref.update({
+                    logged: false
+                });
+            },
+
+            registerNewUserInfo: function (userId, nome, cognome, email, ruolo, citta, nomeFiglio,
+                                           cognomeFiglio /* scuolaFiglio */, classeFiglio, sezioneFiglio) {
+
+                // AGGIUNTA NUOVO UTENTE AL DATABASE
+                var ref = firebase.database().ref().child("utenti").child(userId);
+                var refFiglio1 = firebase.database().ref().child("utenti").child(userId).child("figlio1");
+
+                ref.set({
+                    nome: nome,
+                    cognome: cognome,
+                    email: email,
+                    citta: citta,
+                    ruolo: ruolo
+                });
+
+                refFiglio1.set({
+                    nomeFiglio: nomeFiglio,
+                    cognomeFiglio: cognomeFiglio,
+                    //scuolaFiglio: scuolaFiglio,
+                    classeFiglio: classeFiglio,
+                    sezioneFiglio: sezioneFiglio
+                });
+            }
+        };
+    });
---
+++ components/utenti/utenti.js
@@ -0,0 +1,13 @@
+/**
+ * Created by giuseppegullotta on 04/06/17.
+ */
+
+'use strict';
+
+// utentiService --> RegistrazioneNuovoUtente, LogIn, LogOut
+
+angular.module('puzzle.utenti', [
+    'puzzle.utenti.utentiService',
+])
+
+    .value('version', '0.1');
