ae170fb931fb40e408aaed16dbc74b4367022e7d
giuseppegullotta <giuseppegullotta1994@gmail.com>
2017-06-08 23:04:48 +0200

    PROFILOVIEW: sistemazione Profilo con funzioni che forniscono i dati relativi all'utente loggato

--- ProfiloView/profiloView.js
+++ ProfiloView/profiloView.js
@@ -1,18 +1,38 @@
 /**
- * Created by pamelasiclari on 02/06/17.
+ * Created by giuseppegullotta on 07/06/17.
  */
 
 'use strict';
 
 angular.module('puzzle.profiloView', [
-    'ngRoute',
+    'ngRoute'
 ])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/profiloView', {
-            templateUrl: 'HomepageView/profiloView.html',
-            controller: 'ProfiloCtrl'
+            templateUrl: 'ProfiloView/profiloView.html',
+            controller: 'ProfiloCtrl',
+            resolve: {
+                "currentAuth": ["Auth", function (Auth) {
+                    return Auth.$requireSignIn();
+                }]
+            }
         });
-    }]);
+    }])
 
+    .controller('ProfiloCtrl',
+        ['$scope', '$rootScope', 'CommonProp', 'utenti', 'currentAuth', '$firebaseAuth', '$location',
+            function($scope, $rootScope, CommonProp, utenti, currentAuth, $firebaseAuth, $location) {
+
+                $rootScope.pagina = {};
+                $rootScope.pagina.pagCorrente = 'profiloUtente';
+
+                // ID UTENTE LOGGATO
+                $scope.IDLoggato = currentAuth.uid;
+
+                // DATI UTENTE LOGGATO
+                $scope.utenteRegistrato = {};
+                $scope.utenteRegistrato.user = CommonProp.getUserInfo($scope.IDLoggato);
+
+            }]);
 
